8d03b6e5f5a3693e8b94b13b3b20c98204f99e17
Ming Kong <mkongg92@gmail.com>
2021-01-24 15:01:48 -0500

    comments

--- homework/hw3/mk3574_hw3_q2.cpp
+++ homework/hw3/mk3574_hw3_q2.cpp
@@ -1,20 +1,8 @@
 /*
-Write a program that:
-    Asks the user for their name.
-    Asks the user to input their graduation year.
-    Asks the user to input the current year.
-Assume the student is in a four-year undergraduate program. 
-Display the current status the student is in. 
-Possible status include: not in college yet, freshman, sophomore, junior, senior, graduated. 
-
-Note: If graduation year equals to current year, status is ‘Graduated’; 
-if graduation year is four years after current year, status is ‘Freshman’, etc.
-
-Your program should interact with the user exactly as it shows in the following example:
-Please enter your name: Jessica
-Please enter your graduation year: 2019
-Please enter current year: 2015
-Jessica, you are a Freshman
+Ming Kong
+Tandon Bridge Winter 2021
+Homework 3
+Question 2
 */
 
 #include <iostream>
