ec07d2e70fb78af7ea888552c527782fb8e577aa
Ming Kong <mkongg92@gmail.com>
2021-01-24 13:58:34 -0500

    hw3 q1

---
+++ homework/hw3/mk3574_hw3_q1.cpp
@@ -0,0 +1,66 @@
+/*
+Ming Kong
+
+Question 1:
+Write a program that computes how much a customer has to pay after purchasing two items. 
+The price is calculated according to the following rules:
+    Buy one get one half off promotion: the lower price item is half price.
+    If the customer is club card member, additional 10% off.
+    Tax is added.
+    
+Inputs to the program include:
+Two items’ prices
+Have club card or not (User enters ‘Y’ or ‘y’ for “yes”; ‘N’ or ‘n’ for “no”)
+Tax rate (User enters the percentage as a number; for example they enter 8.25 if the tax rate is 8.25%)
+Program displays: Base price - the price before the discounts and taxes 
+Price after discounts - the price after the buy one get one half off promotion and the member’s discount, if applicable 
+Total price – the amount of money the customer has to pay (after tax)
+
+Your program should interact with the user exactly as it shows in the following example:
+Enter price of first item: 10
+Enter price of second item: 20
+Does customer have a club card? (Y/N): y
+Enter tax rate, e.g. 5.5 for 5.5% tax: 8.25
+Base price: 30.0
+Price after discounts: 22.5
+Total price: 24.35625
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double iOne, iTwo, taxRate, base, discount, total;
+    char club;
+
+    cout << "Enter price of first item: ";
+    cin >> iOne;
+    cout << "Enter price of second item: ";
+    cin >> iTwo;
+    cout << "Does customer have a club card? (Y/N): ";
+    cin >> club;
+    cout << "Enter tax rate, e.g. 5.5 for 5.5% tax: ";
+    cin >> taxRate;
+
+    base = iOne + iTwo;
+
+    if (iOne >= iTwo) {
+        discount = (0.5 * iTwo) + iOne;
+    }
+    else {
+        discount = (0.5 * iOne) + iTwo;
+    }
+
+    if (club == 'y' || club == 'Y') {
+        discount = 0.9 * discount;
+    }
+
+    total = discount * (1 + (taxRate / 100));
+
+    cout << "Base price: " << base << endl;
+    cout << "Price after discounts: " << discount << endl;
+    cout << "Total price: " << total << endl;
+
+    return 0;
+}
