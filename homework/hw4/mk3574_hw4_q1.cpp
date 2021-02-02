a5cf0f031dc73b7fea47d2cee7d6452ebf27656e
Ming Kong <mkongg92@gmail.com>
2021-02-02 14:46:02 -0500

    hw4, q1

---
+++ homework/hw4/mk3574_hw4_q1.cpp
@@ -0,0 +1,37 @@
+/*
+Ming Kong
+Tandon Bridge Winter 2021
+Homework 4
+Question 1
+
+Prints the first n even numbers.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int input;
+    int even = 2;
+
+    cout << "Section a: \n";
+    cout << "Please enter a positive integer: ";
+    cin >> input;
+    while (input > 0) {
+        cout << even << endl;
+        --input;
+        even += 2;
+    }
+
+    cout << "Section b: \n";
+    cout << "Please enter a positive integer: ";
+    cin >> input;
+    even = 2;
+    for (int n = 0; n < input; n++) {
+        cout << even << endl;
+        even += 2;
+    }
+
+    return 0;
+}
